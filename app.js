224b7f43a9a850ae96fe1ae51fe26236ad162aa7
Jirapong Bualuang <jirapong.bualuang@krungthai-axa.co.th>
2022-04-09 23:17:17 +0700

    init commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Asynchronous Operation on Node.js
+// 1. Callbacks -> function name(param1, param2, ..., callback);
+const myRequest = (data, callback) => {
+    const response = 10 + data;
+    const error = undefined;
+    const result = callback(error, response);
+    return result;
+}
+const result = myRequest(5, (error, res) => {
+    if (error) {
+        return 'You have got Error';
+    } else {
+        return res;
+    }
+});
+console.log(result)
+
+// 2. Promise 
+// 3. async/await
+
+console.log('Hello, Node.js');
